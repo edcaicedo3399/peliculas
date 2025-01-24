be207b33309aa147701675e3d70cbf2aca9db9e8
David Caicedo <edcaicedo12@gmail.com>
2025-01-24 12:09:42 -0500

    add file

--- routes/web.js
+++ routes/web.js
@@ -1,16 +1,14 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 const webRouter = Router();
 
 // Servir archivos estáticos desde la carpeta "web"
-webRouter.use('/', (req, res, next) => {
-    express.static(path.join(__dirname, '../web'))(req, res, next);
-});
+webRouter.use(express.static(path.join(path.resolve(), 'web')));
 
 // Ruta raíz: devolver el archivo index.html
 webRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../web', 'index.html'));
+    res.sendFile(path.join(path.resolve(), 'web', 'index.html'));
 });
 
 export { webRouter };
